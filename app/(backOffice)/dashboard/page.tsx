ba549d25b48c85329d235f3311557f6eeed10f99
Enamulx <coderxdante@gmail.com>
2025-04-08 19:18:22 +0600

    updated

--- app/(backOffice)/dashboard/page.tsx
+++ app/(backOffice)/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import Heading from "@/components/backOffice/Heading";
 import LargeCards from "@/components/backOffice/LargeCards";
 import DashboardChart from "@/components/backOffice/DashboardChart";
-import CustomDataTable from "@/components/backOffice/CustomDataTable"
+import CustomDataTable from "@/components/backOffice/CustomDataTable";
 export default function Dashboard() {
 	return (
-		<div className="space-y-6">
+		<div className="space-y-6 h-screen overflow-y-scroll custom-scroll">
 			<Heading title="Dashboard Overview" />
 			{/* cards */}
 			<LargeCards />
