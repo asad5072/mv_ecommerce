c4ca16b470f132421cc435309feb8395c851f962
Enamulx <coderxdante@gmail.com>
2025-04-10 11:10:53 +0600

    updated

--- components/backOffice/CustomDataTable.jsx
+++ components/backOffice/CustomDataTable.jsx
@@ -20,52 +20,31 @@ export default function CustomDataTable() {
 				<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
 					<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 						<tr>
-							<th
-								scope="col"
-								className="p-4"
-							>
+							<th scope="col" className="p-4">
 								<div className="flex items-center">
 									<input
 										id="checkbox-table-search-1"
 										type="checkbox"
 										className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
 									/>
-									<label
-										htmlFor="checkbox-table-search-1"
-										className="sr-only"
-									>
+									<label htmlFor="checkbox-table-search-1" className="sr-only">
 										checkbox
 									</label>
 								</div>
 							</th>
-							<th
-								scope="col"
-								className="px-6 py-3"
-							>
+							<th scope="col" className="px-6 py-3">
 								Product name
 							</th>
-							<th
-								scope="col"
-								className="px-6 py-3"
-							>
+							<th scope="col" className="px-6 py-3">
 								Quantity
 							</th>
-							<th
-								scope="col"
-								className="px-6 py-3"
-							>
+							<th scope="col" className="px-6 py-3">
 								Price
 							</th>
-							<th
-								scope="col"
-								className="px-6 py-3"
-							>
+							<th scope="col" className="px-6 py-3">
 								ID
 							</th>
-							<th
-								scope="col"
-								className="px-6 py-3"
-							>
+							<th scope="col" className="px-6 py-3">
 								Action
 							</th>
 						</tr>
@@ -138,12 +117,24 @@ export default function CustomDataTable() {
 								Previous
 							</button>
 						</li>
+						{Array.from({ length: totalPages }, (_, index) => {
+							return (
+								<li key={index}>
+									<button
+										onClick={() => setCurrentPage(index + 1)}
+										disabled={currentPage == index + 1}
+										className={
+											currentPage == index + 1
+												? "flex items-center justify-center px-3 h-8 leading-tight text-white-500 bg-blue-500 border border-blue-500 hover:bg-blue-600 hover:text-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+												: "flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+										}
+									>
+										{index + 1}
+									</button>
+								</li>
+							);
+						})}
 
-						<li>
-							<button className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-								1
-							</button>
-						</li>
 						<li>
 							<button
 								onClick={() => setCurrentPage(currentPage + 1)}
